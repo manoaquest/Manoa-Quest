8bdf8274b758b3436787f525013d057eeb834f17
evanyasaka <eyasaka@hawaii.edu>
2017-04-18 09:18:08 -1000

    Remade Repo
    Had to remake the repo to support issue creation and project management.

---
+++ app/imports/startup/client/router.js
@@ -0,0 +1,43 @@
+import { FlowRouter } from 'meteor/kadira:flow-router';
+import { BlazeLayout } from 'meteor/kadira:blaze-layout';
+
+FlowRouter.route('/', {
+  name: 'Landing_Page',
+  action() {
+    BlazeLayout.render('App_Body', { main: 'Landing_Page' });
+  },
+});
+
+FlowRouter.route('/approve-quest', {
+  name: 'Approve_Quest_Page',
+  action() {
+    BlazeLayout.render('App_Body', { main: 'Approve_Quest_Page' });
+  },
+});
+
+FlowRouter.route('/create-quest', {
+  name: 'Create_A_Quest_Page',
+  action() {
+    BlazeLayout.render('App_Body', { main: 'Create_A_Quest_Page' });
+  },
+});
+
+FlowRouter.route('/student-home-page', {
+  name: 'Student_Home_Page',
+  action() {
+    BlazeLayout.render('App_Body', { main: 'Student_Home_Page' });
+  },
+});
+
+FlowRouter.route('/teacher-page', {
+  name: 'Teacher_Page',
+  action() {
+    BlazeLayout.render('App_Body', { main: 'Teacher_Page' });
+  },
+});
+
+FlowRouter.notFound = {
+  action() {
+    BlazeLayout.render('App_Body', { main: 'App_Not_Found' });
+  },
+};
---
+++ app/imports/ui/pages/index.js
@@ -0,0 +1,19 @@
+/*------------------------------------------------------------------
+When adding imports order alphabetically to make finding them in the
+file tree easier.
+------------------------------------------------------------------*/
+
+import './approve-quest-page.html';
+import './approve-quest-page.js';
+
+import './create-a-quest-page.html';
+import './create-a-quest-page.js';
+
+import './landing-page.html';
+import './landing-page.js';
+
+import './student-home-page.html';
+import './student-home-page.js';
+
+import './teacher-page.html';
+import './teacher-page.js';
